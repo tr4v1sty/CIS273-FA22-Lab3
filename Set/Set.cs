cae17ef333106ed439e6075f043bd1cf3da18d91
tr4v1sty <travis.rbusy@gmail.com>
2022-12-05 16:26:13 -0600

    Set functions now work

--- Set/Set.cs
+++ Set/Set.cs
@@ -8,22 +8,62 @@ namespace Set
 {
     public class Set<T> : ISet<T>
     {
-        HashSet<T> set;
+        private HashSet<T> set;
+        //forgot to make a public set
+        public Set()
+        {
+            set = new HashSet<T>();
+        }
         public int Size => set.Count;
 
-        public List<T> Elements { get; set; }
+
+        // get set did not work for elements :(
+        // create a list of elements to pull from
+        public List<T> Elements
+        {
+            get
+            {
+                List<T> list = new List<T>();
+                foreach (var item in set)
+                {
+                    list.Add(item);
+                }
+                return list;
+            }
+        }
 
         public static ISet<int> Intersection(ISet<int> set1, ISet<int> set2)
         {
-            throw new NotImplementedException();
+            Set<int> newSet = new Set<int>();
+            // for each element in a set if they match add them to a new set
+            foreach (var element1 in set1)
+            {
+                foreach (var element2 in set2)
+                {
+                    if (element1.Equals(element2))
+                    {
+                        newSet.Add(element1);
+                    }
+                }
+            }
+            return newSet;
         }
 
         public static ISet<int> Union(ISet<int> set1, ISet<int> set2)
         {
-            
-            throw new NotSupportedException();
+            // add all elements to a set
+            Set<int> newSet = new Set<int>();
+            foreach (var element1 in set1)
+            {
+                newSet.Add(element1);
+            }
+            foreach (var element2 in set2)
+            {
+                newSet.Add(element2);
+            }
+            return newSet;
         }
-
+        // basic functions
         public void Add(ISet<T> s)
         {
             foreach (var thing in s)
@@ -31,12 +71,10 @@ namespace Set
                 set.Add(thing);
             }
         }
-
         public void Add(T value)
         {
             set.Add(value);
         }
-
         public bool Contains(T value)
         {
             if (set.Contains(value) == true)
@@ -49,17 +87,15 @@ namespace Set
 
         public IEnumerator<T> GetEnumerator()
         {
-            throw new NotImplementedException();
+            return set.GetEnumerator();
         }
 
         public void Remove(ISet<T> s)
         {
             foreach (var thing in s) 
             {
-                set.Remove(thing);
-                    
+                set.Remove(thing);  
             }
-
         }
 
         public void Remove(T value)
@@ -69,7 +105,7 @@ namespace Set
 
         IEnumerator IEnumerable.GetEnumerator()
         {
-            throw new NotImplementedException();
+            return set.GetEnumerator();
         }
     }
 }
